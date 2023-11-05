f81a34b30cf5259da75ff21a446802df6476409f
eneassena <desenvolvedoreneas2000@gmail.com>
2023-11-05 02:33:44 -0300

    remove folders in theme

--- theme/site/fragmentos/home/card.body.php
+++
@@ -1,5 +0,0 @@
-  <?php if (isset($_SESSION['person_comun']['on'])) : ?>
-    <a href="<?= url('pedido') ?>" class="card-link btn btn-warning">Pedir</a>
-  <?php else : ?>
-    <a id="maravilha_btn" title="Faça login para fazer um pedido" href="#" class="card-link btn btn-warning">Pedir</a>
-  <?php endif ?>
